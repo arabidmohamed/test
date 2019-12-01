cff19b1bb47af256420fad061750e0c9a04d9aec
arabidmohamed <arabidmohamed@gmail.com>
2019-12-01 14:09:29 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,54 @@
+
+<?php 
+
+
+$var1 = 2;
+
+$var2 = 8.5;
+
+$var3 = "PHP Course";
+
+$var4 = 'PHP 5.6';
+
+echo $var3.' '.$var4.'<br>';
+
+
+echo $var1 + $var2.'<br>';
+
+
+// $var1++;
+// $var2--;
+
+$var1 +=8;
+
+$var2 -=2;  // $var2 = $var2 - 2;
+
+
+echo $var1;
+
+
+//echo "Hi There";   
+
+
+
+
+
+?>
+
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>PHP Course </title>
+</head>
+<body>
+				<div style="color: red; font-size:28px; ">
+				<?= $var4; ?>
+				</div>
+				<div style="color: green; font-size:32px; font-weight: 600;">
+					<?= $var1; ?>
+				</div>
+</body>
+</html>
+
+
